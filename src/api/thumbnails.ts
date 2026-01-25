124d62facb86e1b57e75734db28af572a44782fc
esoys <erik@soysal.de>
2026-01-25 17:22:21 +0100

    add cache

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,16 +1,20 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
+import path from "node:path";
+
+
+const MAX_UPLOAD_SIZE = 10 << 20;
+
 
 type Thumbnail = {
   data: ArrayBuffer;
   mediaType: string;
 };
 
-const videoThumbnails: Map<string, Thumbnail> = new Map();
 
 export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -48,6 +52,33 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
   // TODO: implement the upload here
+    const formData = await req.formData();
+    const file = formData.get("thumbnail");
+    if (!(file instanceof File)) {
+        throw new BadRequestError("Thumbnail file is missing");
+    }
 
-  return respondWithJSON(200, null);
+    if (file.size > MAX_UPLOAD_SIZE) {
+        throw new BadRequestError("File size exceeds maximum updaload size");
+    }
+
+    const mediaType = file.type;
+    if (!(mediaType === "image/png" || mediaType === "image/jpeg")) {
+        throw new BadRequestError("Wrong file format");
+    }
+    const mediaData = await file.arrayBuffer();
+    const filePath = path.join(cfg.assetsRoot, `${videoId}.${mediaType}`);
+    
+    Bun.write(filePath, mediaData);
+
+    let mediaMetadata = await getVideo(cfg.db, videoId);
+    if (mediaMetadata.userID !== userID) {
+        throw new UserForbiddenError("Access to resource denied");
+    }
+ 
+    mediaMetadata.thumbnailURL = `http://localhost:${cfg.port}/assets/${videoId}.${mediaType}`;
+
+    await updateVideo(cfg.db, mediaMetadata);
+
+  return respondWithJSON(200, mediaMetadata);
 }
